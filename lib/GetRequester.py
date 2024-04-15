06faa5df732b35943ab08ee5d26f077afb717215
zakariamohamed123 <zakaria.mohamed1@student.moringaschool.com>
2024-04-15 13:11:42 +0300

    complete

---
+++ lib/GetRequester.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+class GetRequester:
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_response_body(self):
+        pass
+
+    def load_json(self):
+        pass
